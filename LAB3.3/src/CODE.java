4c786f40abc6740e8341367f96c555fd6d3cfd17
mrpresident2018 <sbansal3625@bths.edu>
2018-02-18 09:48:33 -0500

    Idk if this works cause my comp is weird

--- LAB3.3/src/CODE.java
+++ LAB3.3/src/CODE.java
@@ -8,93 +8,148 @@ public class CODE
 	String [] L2 = {"cherry" , "mahogany" , "oreos" , "pinata"};
 	int [] L3 = {3,2,4,65,23,145,66,234,1234,6,24,53};
 	
-	System.out.print()
+	long start;
+	long end;
+	long time;
 	
 	//
 	//Merge Test
-	long start = System.nanoTime();
+	start = System.nanoTime();
 	String[] mergeResult = mergeSort(L1);
-	long end = System.nanoTime();
-	long time = end - start;
-	System.out.println();
+	end = System.nanoTime();
+	time = end - start;
+	System.out.println("MergeSort test took: " + time + " nanoseconds");
 	System.out.println(mergeResult);
 	}
-	//Partition
-	/*long start = System.nanoTime();
-	int pivotFinalPos = partition(L3);
-	long end = System.nanoTime();
-	long time = end - start;
-	System.out.println("Partition test took: " + time + " nanoseconds");
-	System.out.println("Final Pivot Position: " + pivotFinalPos);
-	}
-	*/
+	//quickSort result
+	start = System.nanoTime();
+	quickSort (L3, 0, L3.length);
+	end = System.nanoTime;
+	time = end - start;
+	System.out.println("QuickSort test took: " + time + " nanoseconds");
+	System.out.println(L3);
 	public static String[] mergeSort(String[]L1)
 	{
 		//Base code
-		int a = L1.length;
-		String[] left = new String[(a)/2];
-		String[] right = new String[a - left.length];
-		if (a <= 1)
+		if(L1.length == 1)
 		{
 			return L1;
 		}
-		return (merge(mergeSort(right) , mergeSort(left)));
+		int a = (L1.length/2);
+		String [] L = (String[]) Arrays.copyOfRange(L1, 0, a);
+		String [] R = (String[]) Arrays.copyOfRange(L1, a, L1.length);
+		
+		return (merge(mergeSort(R) , mergeSort(L)));
 	}
 	public static String[] merge(String [] L1, String [] L2) 
 	{
-		String [] f = new String [(L1.length) + (L2.length)-1]; 
+		String [] f = new String [(L1.length) + (L2.length)]; 
 		int a = 0;
 		int b = 0;
 		int c = 0;
-		while(b < L1.length && c < L2.length && a < f.length) 
+		while(a < L1.length && b < L2.length) 
 		{
-			if (L1[b].compareTo(L2[c]) >= 0)
+			if (L1[a].compareTo(L2[b]) <= 0)
 			{
-				f[a] = L2[c];
+				f[c] = L1[a];
 				a++;
-				c++;
 			}				
-			else
+			else if (L1[a].compareTo(L2[b]) > 0)
 			{
-				f[a] = L1[b];
-				a++;
-				b++;
+				f[c] = L2[b];
+				c++;
 			}
+		}
+		while (a < L1.length)
+		{
+			f[c] = L1[a];
+			a++;
+			c++;
+		}
+		while (b < L2.length)
+		{
+			f[c] = L2[b];
+			b++;
+			c++;
 		}
 		return f;
 	}
 	public static int partition(int[]L) //Partition
 	{
-		int B = L[0];    
-	    int T = 0;
-	    int n = 0;
-	    int x = L.length -1;
-
-	    for(int i = 1; i < L.length; i++)
+		int d = L[0];    
+	    int e = 0;
+	    int f = L.length -1;
+	    while (f != e)
 	    {
-	        if( L[i] <= (B))
-	        {
-	            swap(L, i, B);           
-	            n = i;
-	        } 
-	        else if(L[i] > (B) && x > i)
-	        {
-	            T = L[i];
-	            L[i] = L[x];
-	            L[x] = T;
-	            x = 1;
-	            i--;
-	        }
+	    	while (e < f)
+	    	{
+	    		if(L[f] >= d)
+	    		{
+	    			f--;
+	    		}
+	    		
+	    		else if (L[f] < e)
+	    		{
+	    			int s = L[e];
+	    			L[e] = L[f];
+	    			L[f] = s;
+	    			
+	    			int sp = f;
+	    			f = e;
+	    			e = sp;
+	    			f++;
+	    		}
+	    	}
 	    }
-	    
-	    System.out.println(java.util.Arrays.toString(L));
-	    System.out.println(n);
-	    return n;
+	    while (e > f)
+	    {
+	    	if (L[f] <= d)
+	    	{
+	    		f++;
+	    	}
+	    	else if (L[f] > d)
+	    	{
+	    		int s = L[e];
+    			L[e] = L[f];
+    			L[f] = s;
+    			
+    			int sp = f;
+    			f = e;
+    			e = sp;
+    			f--;
+	    	}
+	    }
+	    return e;
 	}
-	private static void swap (int [] L , int i , int b)
+	public static void quickSort(int[] L, int f, int b)
 	{
-		int t = L[i];
-		L[i] = L[b];
-		L[b] = t;
+		if (b > f)
+		{
+			int pIndex = partition(L, f, b);
+			
+			quickSort(L, f, pIndex);
+			quickSort(L, pIndex + 1, b);
+		}
+	}
+	public static int partition (int[] L, int f, int b)
+	{
+		int pi = L[f];
+		int hp = b-1;
+		for (int x = hp; x >= 1; x--)
+		{
+			if (L[x] > pi)
+			{
+				if (x != hp)
+				{
+					int sb = L[hp];
+					L[hp] = L[x];
+					L[x] = sb;
+				}
+				hp--;
+			}
+		}
+		L[f] = L[hp];
+		L[hp] = pi;
+		return hp;
 	}
 }
